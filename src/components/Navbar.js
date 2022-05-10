a6634b1207f27f6dc1187bb98a543067926d5e85
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2022-05-10 16:29:33 +0300

    Add new classes to some elements od nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,18 @@
-import react from "react";
+import React from "react";
 import planet  from '../img/planet.png'
 
 const Navbar = () => {
     return (
         <nav>
-            <div>
-                <img className="logo-img" src={planet} alt="logo" />
-                <h1>Space Travellers' Hub</h1>
+            <div className="title">
+              <div><img className="logo-img" src={planet} alt="logo" /></div>
+              <h1>Space Travellers' Hub</h1>
             </div>
             <div>
                 <ul>
                     <li>Rockets</li>
                     <li>Missions</li>
-                    <li>My Profile</li>
+                    <li className="profile">My Profile</li>
                 </ul>
             </div>
         </nav>
