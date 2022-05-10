9cd6c022dea05940f141c40352cca6fd5cde373f
fed1k <allamurotovcoder@mail.ru>
2022-05-10 20:20:38 +0500

    FIx linter issues

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,24 @@
 import React from "react";
-import planet  from '../img/planet.png'
+import planet from "../img/planet.png";
 
 const Navbar = () => {
-    return (
-        <nav>
-            <div className="title">
-              <div><img className="logo-img" src={planet} alt="logo" /></div>
-              <h1>Space Travellers' Hub</h1>
-            </div>
-            <div>
-                <ul>
-                    <li>Rockets</li>
-                    <li>Missions</li>
-                    <li className="profile">My Profile</li>
-                </ul>
-            </div>
-        </nav>
-    );
-}
- 
+  return (
+    <nav>
+      <div className="title">
+        <div>
+          <img className="logo-img" src={planet} alt="logo" />
+        </div>
+        <h1>Space Travellers' Hub</h1>
+      </div>
+      <div>
+        <ul>
+          <li>Rockets</li>
+          <li>Missions</li>
+          <li className="profile">My Profile</li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
 export default Navbar;
