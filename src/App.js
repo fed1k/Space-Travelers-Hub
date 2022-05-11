0d3baa8421a977afcaaf65c534d2cf72e8addf9a
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2022-05-11 23:16:00 +0300

    Add missions to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { React, useEffect } from 'react';
 import './index.scss';
+import { useDispatch } from 'react-redux';
 import Navbar from './components/Navbar';
+import Missions from './missions/missionPage';
+import { fetchMissions } from './missions/missions';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchMissions());
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <Navbar />
+        <Missions />
       </header>
     </div>
   );
