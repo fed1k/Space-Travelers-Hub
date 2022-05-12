1e357e634b3272d91073797ae60b8ab6e39199e0
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2022-05-12 19:49:20 +0300

    Add navlink active class style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,9 +12,41 @@ const Navbar = () => (
     </div>
     <div>
       <ul>
-        <li><NavLink to="/">Rockets</NavLink></li>
-        <li><NavLink to="/missions">Missions</NavLink></li>
-        <li className="profile"><NavLink to="/myprofile">My Profile</NavLink></li>
+        <li>
+          <NavLink
+            className="link"
+            to="/"
+            style={({ isActive }) => ({
+              textDecoration: isActive ? 'underline' : 'none',
+            })}
+          >
+            Rockets
+          </NavLink>
+
+        </li>
+        <li>
+          <NavLink
+            to="/missions"
+            className="link"
+            style={({ isActive }) => ({
+              textDecoration: isActive ? 'underline' : 'none',
+            })}
+          >
+            Missions
+          </NavLink>
+        </li>
+        <li className="profile">
+          <NavLink
+            to="/myprofile"
+            className="link"
+            style={({ isActive }) => ({
+              textDecoration: isActive ? 'underline' : 'none',
+            })}
+          >
+            My Profile
+          </NavLink>
+
+        </li>
       </ul>
     </div>
   </nav>
