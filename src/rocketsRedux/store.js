0e5d20fd252ce937afa3a746fcf7379bf5d3bcbd
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2022-05-13 15:09:09 +0300

    Resolve merge conflicts

--- src/rocketsRedux/store.js
+++ src/rocketsRedux/store.js
@@ -2,6 +2,6 @@ import { legacy_createStore as createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { myReducer } from './fetchData';
 
-const rocketstore = createStore(myReducer, applyMiddleware(thunk));
+const store = createStore(myReducer, applyMiddleware(thunk));
 
-export default rocketstore;
+export default store;
